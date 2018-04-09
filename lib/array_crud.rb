e2f246b2249df00ca4aa012216f4fe12cfd53484
Dani Lyn <dlyn1110@gmail.com>
2018-04-09 15:37:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-array.pop("arrays!")
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array.shift("wow")
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -31,5 +31,5 @@ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+array[1]
 end
